fd0fba5a9f5e03a5a125fa58f7dbcbe6807ff896
Mohamed Diop <mohdiopcode@gmail.com>
2025-07-03 10:45:48 +0000

    Initialise le projet

---
+++ src/main/java/com/groupe1/collabdev_api/CollabdevApiApplication.java
@@ -0,0 +1,13 @@
+package com.groupe1.collabdev_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CollabdevApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CollabdevApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/groupe1/collabdev_api/CollabdevApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.groupe1.collabdev_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CollabdevApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
