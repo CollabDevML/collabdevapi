be8d3eca8d09c35421276df20e55b96cc55cfcd9
Abdoul03 <samabdoul03@gmail.com>
2025-07-04 11:20:06 +0000

    creation du projet par le gestionnaire

---
+++ src/main/java/com/groupe1/collabdev_api/controllers/GestionnaireController.java
@@ -0,0 +1,39 @@
+package com.groupe1.collabdev_api.controllers;
+
+import com.groupe1.collabdev_api.entities.Gestionnaire;
+import com.groupe1.collabdev_api.repositories.GestionnaireRepository;
+import com.groupe1.collabdev_api.services.GestionnaireService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/gestionnaire")
+@RequiredArgsConstructor
+public class GestionnaireController {
+    @Autowired
+    private GestionnaireService gestionnaireService;
+
+    @PostMapping
+    public Gestionnaire ajouterUnGestionnair(@RequestBody Gestionnaire gestionnaire){
+        return gestionnaireService.ajouter(gestionnaire);
+    }
+
+    @GetMapping
+    public List<Gestionnaire> afficherLesGestionnaire(){
+        return gestionnaireService.chercherTous();
+    }
+
+    @GetMapping("/{id}")
+    public Gestionnaire afficherUnGestionnaire(@PathVariable int id){
+         return gestionnaireService.chercherParId(id);
+    }
+
+    @DeleteMapping("/{id}")
+    public boolean SupprimerUnGestionnaire(@PathVariable int id){
+        gestionnaireService.supprimerParId(id);
+        return true;
+    }
+}
--- src/main/java/com/groupe1/collabdev_api/repositories/ProjetRepository.java
+++ src/main/java/com/groupe1/collabdev_api/repositories/ProjetRepository.java
@@ -4,6 +4,11 @@ package com.groupe1.collabdev_api.repositories;
 import com.groupe1.collabdev_api.entities.Projet;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface ProjetRepository extends JpaRepository<Projet, Integer>
 {
+    List<Projet> findByGestionnaireId(int gestionnaireId);
+    Optional<Projet> findByIdAndGestionnaireId(Integer id, Integer gestionnaireId);
 }
