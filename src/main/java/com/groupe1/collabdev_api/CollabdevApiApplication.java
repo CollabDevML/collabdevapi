1d9adb89162dd7f8cf049a307a093195f1f8f82e
Amadou Bagayoko <abagayoko304@gmail.com>
2025-07-04 00:40:08 +0000

    ajout du controller

--- src/main/java/com/groupe1/collabdev_api/CollabdevApiApplication.java
+++ src/main/java/com/groupe1/collabdev_api/CollabdevApiApplication.java
@@ -4,8 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class
-CollabdevApiApplication {
+public class CollabdevApiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CollabdevApiApplication.class, args);
