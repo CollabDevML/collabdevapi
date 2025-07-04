bb47f5a804a9f4b1a036722c58202b03315ff634
jimlecracken14 <jimlecracken13@gmail.com>
2025-07-04 12:00:19 +0000

    demandecontribution ok

--- src/main/java/com/groupe1/collabdev_api/services/DemandeContributionService.java
+++ src/main/java/com/groupe1/collabdev_api/services/DemandeContributionService.java
@@ -4,7 +4,6 @@ import com.groupe1.collabdev_api.entities.DemandeContribution;
 import com.groupe1.collabdev_api.repositories.DemandeContributionRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
 import java.util.List;
 
 @Service
@@ -29,9 +28,29 @@ public class DemandeContributionService {
         return demandeContributionRepository.save(demandeContribution);
     }
 
+    public DemandeContribution modifier(int id, DemandeContribution demandeContribution)
+    {
+        demandeContribution.setId(id);
+        return demandeContributionRepository.save(demandeContribution);
+    }
+
     public Boolean supprimerParId(int id){
         demandeContributionRepository.deleteById(id);
         return true;
     }
+
+    //lister demande d'un contributeur
+    public List<DemandeContribution> chercherParContributeur(int idContributeur)
+    {
+        return demandeContributionRepository.findByContributeur_Id(idContributeur);
+    }
+
+    //lister demande par projet
+    public List<DemandeContribution> chercherParProjet(int idProjet)
+    {
+        return demandeContributionRepository.findByProjet_Id(idProjet);
+    }
+
+    //afficher la liste des demandes validées
 }
 
