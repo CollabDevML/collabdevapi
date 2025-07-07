ba839b0e48f1c7335ffcd9a8d3f9dad8278c1a51
Mohamed Diop <mohdiopcode@gmail.com>
2025-07-07 12:35:21 +0000

    Revoie le code

--- src/main/java/com/groupe1/collabdev_api/controllers/PorteurProjetController.java
+++ src/main/java/com/groupe1/collabdev_api/controllers/PorteurProjetController.java
@@ -11,7 +11,7 @@ import java.util.List;
 import java.util.Optional;
 
 @RestController
-@RequestMapping("/porteurs_projet")
+@RequestMapping("/utilisateurs/porteurs-projet")
 public class PorteurProjetController {
     @Autowired
     private PorteurProjetService porteurProjetService;
